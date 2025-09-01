881e6661f7707f7901b87fc506c0f4f1ccc278ce
Benedek Pal <benedek.pal1024@gmail.com>
2025-09-01 16:39:04 +0200

    created server after running reachable at port 8080

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	return
+	// Create a new ServeMux
+	mux := http.NewServeMux()
+
+	// Create the http.Server struct
+	server := &http.Server{
+		Addr:    ":8080", // Listen on port 8080
+		Handler: mux,     // Use the custom ServeMux as the handler
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("error starting server: %v", err)
+	}
 }
